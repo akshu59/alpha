c9a6d67fdee8eda231b8b42bdc646340338ba260
akshu59 <109548322+akshu59@users.noreply.github.com>
2023-01-04 10:58:55 +0530

    Add files via upload

---
+++ src/Components/NavBar.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./NavBar.css";
+
+const Navbar = () => {
+  return (
+    <ul>
+      <li className="rcorners">Home</li>
+      <li className="rcorners">ArtGallery</li>
+      <li className="rcorners">GameMania</li>
+      <li className="rcorners">ARWorld</li>
+      <li className="rcorners">LetsCreate</li>
+      <li className="rcorners">About</li>
+    </ul>
+  );
+};
+
+export default Navbar;
